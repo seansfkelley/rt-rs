215565f3d6fc907f2f4d35d4ad23ce04343de994
Kevin Verdieck <kevinverdieck@gmail.com>
2017-10-25 17:48:17 -0700

    Add Vec3 and cross product

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@
 extern crate log;
 extern crate env_logger;
 
+mod vector;
+
+use vector::Vec3;
+
 fn main() {
     env_logger::init().unwrap();
 
@@ -10,4 +14,9 @@ fn main() {
     warn!("warn");
 
     println!("Hello, world!");
+
+    let normal = Vec3::new(1f64, 2f64, 3f64);
+    let ray = Vec3::new(4f64, 5f64, 6f64);
+    let thing = normal.cross(&ray);
+    println!("normal: {:?}, ray: {:?}, result: {:?}", normal, ray, thing);
 }
---
+++ src/vector.rs
@@ -0,0 +1,23 @@
+#[derive(Debug)]
+pub struct Vec3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Vec3 {
+        Vec3 { x, y, z }
+    }
+
+    pub fn cross(&self, other: &Vec3) -> Vec3 {
+        // cx = aybz − azby
+        // cy = azbx − axbz
+        // cz = axby − aybx
+        Vec3 {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
+    }
+}
