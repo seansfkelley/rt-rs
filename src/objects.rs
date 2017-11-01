74fa78480f4b3eb05487e527db5b3e4b14d40565
Sean Kelley <seansfkelley@gmail.com>
2017-10-31 17:51:17 -0700

    SOLVE TODOS

--- src/objects.rs
+++ src/objects.rs
@@ -136,15 +136,12 @@ impl SceneObject for SubtractedSceneObject {
                 let rhs_hit_option = self.rhs.intersect(ray);
                 match rhs_hit_option {
                     Some(rhs_hit) => {
-                        let lhs_enter = lhs_hit.enter.map(|enter| enter.distance).unwrap_or(0f64);
+                        let lhs_enter = lhs_hit.enter.as_ref().map(|enter| enter.distance).unwrap_or(0f64);
                         let rhs_enter = rhs_hit.enter.map(|enter| enter.distance).unwrap_or(0f64);
                         let lhs_exit = lhs_hit.exit.distance;
                         let rhs_exit = rhs_hit.exit.distance;
                         if lhs_enter < rhs_enter || lhs_enter > rhs_exit {
-                            // Oh god
-                            // TODO: borrow
-                            // lhs_hit_option
-                            self.lhs.intersect(ray)
+                            Some(lhs_hit)
                         } else {
                             // lhs_enter is inside rhs
                             if rhs_exit < lhs_exit {
@@ -164,10 +161,7 @@ impl SceneObject for SubtractedSceneObject {
                             }
                         }
                     },
-                    // Oh god
-                    // TODO: borrow
-                    // lhs_hit_option
-                    None => self.lhs.intersect(ray),
+                    None => Some(lhs_hit),
                 }
             },
             None => None,
