ef587907714f156ab9beb637eddc8dc15fe6ac1b
Kevin Verdieck <kevinverdieck@gmail.com>
2017-11-09 19:44:06 -0800

    Yoink spheres from master

--- src/geometry/sphere.rs
+++ src/geometry/sphere.rs
@@ -6,6 +6,7 @@ use transform::Mat4;
 use material::Material;
 use geometry::Geometry;
 use util::Clamp;
+use math::Vec3;
 
 #[derive(Debug)]
 pub struct Sphere {
@@ -14,59 +15,82 @@ pub struct Sphere {
 
 impl Sphere {
     pub fn new(radius: f64) -> Sphere {
-        Sphere { radius }
+        Sphere {
+            radius,
+        }
     }
 
     fn get_intersection(&self, t: f64, ray: &Ray) -> Intersection {
-        let location = ray.at(t);
+        let intersection_point = ray.at(t);
 
         // pbrt pg. 119
-        // Make sure we transform into object space!
-        let mut phi = location.y.atan2(location.x);
+        let mut phi = intersection_point.y.atan2(intersection_point.x);
         if phi < 0f64 {
             phi += 2f64 * PI;
         }
-        let theta = (location.z / self.radius).clamp(-1f64, 1f64).acos();
+        let theta = (intersection_point.z / self.radius).acos();
 
         Intersection {
             distance: t,
-            location,
-            normal: location.as_unit_vector(),
+            location: intersection_point,
+            normal: intersection_point.as_unit_vector(),
             uv: (phi / (2f64 * PI), theta / PI),
+        }
+    }
+}
+
+// pbrt pg. 118
+fn quadratic(a: f64, b: f64, c: f64) -> Option<(f64, f64)> {
+    let d = b * b - 4f64 * a * c;
+    if d <= 0f64 {
+        None
+    } else {
+        let sqrt_d = d.sqrt();
+        let q = -0.5f64 * (b + (if b < 0f64 { -sqrt_d } else { sqrt_d }));
+        let (t0, t1) = (q / a, c / q);
+        if t0 > t1 {
+            Some((t1, t0))
+        } else {
+            Some((t0, t1))
         }
     }
 }
 
 impl Geometry for Sphere {
     fn intersect(&self, ray: &Ray) -> Option<Hit> {
-        let l = ray.origin;
-        let t_center = l.dot(ray.direction);
-        if t_center + self.radius <= 0f64 {
-            None
-        } else {
-            let d_sq = l.magnitude2() - t_center * t_center;
-            let r_sq = self.radius * self.radius; // could cache?
-            if d_sq > r_sq {
-                None
-            } else {
-                let t_distance = (r_sq - d_sq).sqrt();
-                let t0 = t_center - t_distance;
-                let t1 = t_center + t_distance;
-                let exit = self.get_intersection(t1, ray);
-                if t0 <= 0f64 {
-                    Some(Hit {
-                        enter: None,
-                        exit,
-                        debug: false,
-                    })
+        let (a, b, c) = (
+            ray.direction.magnitude2(),
+            2f64 * (ray.direction.dot(ray.origin)),
+            ray.origin.dot(ray.origin) - self.radius * self.radius
+        );
+
+        let enter = match quadratic(a, b, c) {
+            Some((t0, t1)) => {
+                if t1 < 0f64 {
+                    None
+                } else if t0 < 0f64 {
+                    Some(self.get_intersection(t1, &ray))
                 } else {
-                    Some(Hit {
-                        enter: Some(self.get_intersection(t0, ray)),
-                        exit,
-                        debug: false,
-                    })
+                    Some(self.get_intersection(t0, &ray))
                 }
-            }
+            },
+            None => None,
+        };
+
+        match enter {
+            Some(intersection) => {
+                Some(Hit {
+                    enter: Some(intersection),
+                    exit: Intersection {
+                        distance: -100f64,
+                        location: Vec3::new(0f64, 0f64, 0f64),
+                        normal: Vec3::new(0f64, 0f64, 0f64),
+                        uv: (0f64, 0f64),
+                    },
+                    debug: false
+                })
+            },
+            None => None,
         }
     }
 }
