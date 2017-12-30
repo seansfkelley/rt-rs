c23ef1a4a8c4e2c23f9d560d7c4f7527af45e76b
Sean Kelley <seansfkelley@gmail.com>
2017-12-29 23:53:26 -0500

    Scope down intermediate variables in PhongMaterial for safety.

--- src/material/phong_material.rs
+++ src/material/phong_material.rs
@@ -15,20 +15,26 @@ impl Material for PhongMaterial {
         let uv = intersection.uv;
         let mut bxdfs: Vec<Box<Bxdf>> = vec![];
 
-        let diffuse = self.diffuse.get_color(uv);
-        if diffuse.is_nonzero() {
-            bxdfs.push(Box::new(Lambertian::new(diffuse)));
+        {
+            let diffuse = self.diffuse.get_color(uv);
+            if diffuse.is_nonzero() {
+                bxdfs.push(Box::new(Lambertian::new(diffuse)));
+            }
         }
 
-        let reflection = self.reflection.get_color(uv);
-        if reflection.is_nonzero() {
-            bxdfs.push(Box::new(PerfectSpecularReflection::new(reflection)));
+        {
+            let reflection = self.reflection.get_color(uv);
+            if reflection.is_nonzero() {
+                bxdfs.push(Box::new(PerfectSpecularReflection::new(reflection)));
+            }
         }
 
-        let transmission = self.transmission.get_color(uv);
-        if transmission.is_nonzero() {
-            // TODO: pbrt has these two IOR flipped the other way, but why?
-            bxdfs.push(Box::new(PerfectSpecularTransmission::new(transmission, 1f64, self.index_of_refraction)));
+        {
+            let transmission = self.transmission.get_color(uv);
+            if transmission.is_nonzero() {
+                // TODO: pbrt has these two IOR flipped the other way, but why?
+                bxdfs.push(Box::new(PerfectSpecularTransmission::new(transmission, 1f64, self.index_of_refraction)));
+            }
         }
 
         Bsdf::new(bxdfs, intersection)
